b31b5d0375c3a2dc3471cce7b550e74385ceb432
Stefan Ferstl <st.ferstl@gmail.com>
2015-05-27 19:48:35 +0200

    Add chronicle for inbound messages

--- src/main/java/com/github/ferstl/processing/ProcessingConfiguration.java
+++ src/main/java/com/github/ferstl/processing/ProcessingConfiguration.java
@@ -1,8 +1,25 @@
 package com.github.ferstl.processing;
 
+import java.io.File;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import net.openhft.chronicle.Chronicle;
+import net.openhft.chronicle.ChronicleQueueBuilder;
 
 @Configuration
+@PropertySource("classpath:processing.properties")
 public class ProcessingConfiguration {
 
+  @Autowired
+  private Environment env;
+
+  @Bean
+  public Chronicle inboundJournal() throws IOException {
+    String inboundJournal = this.env.getProperty("inbound.journal");
+    return ChronicleQueueBuilder.indexed(new File(inboundJournal)).synchronous(false).messageCapacity(10000).build();
+  }
 }
