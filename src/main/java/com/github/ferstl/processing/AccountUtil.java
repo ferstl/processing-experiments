abe2d85f8b5d598973729f4c77fd730bf8dfbee4
Stefan Ferstl <st.ferstl@gmail.com>
2020-05-21 14:58:10 +0200

    Introduce utility to get random accounts and amounts

---
+++ src/main/java/com/github/ferstl/processing/AccountUtil.java
@@ -0,0 +1,42 @@
+package com.github.ferstl.processing;
+
+import java.math.BigDecimal;
+import java.util.Random;
+import org.jetbrains.annotations.NotNull;
+import static com.github.ferstl.processing.AccountMasterdata.getRegularAccountsAsArray;
+
+public class AccountUtil {
+
+  private static final int NR_OF_AMOUNTS = 10000;
+  private static final String[] accounts;
+  private static final String[] amounts;
+  private static final Random random;
+
+  static {
+    random = new Random(42);
+    accounts = getRegularAccountsAsArray();
+
+    int[] integers = random.ints(100, 10000)
+        .limit(NR_OF_AMOUNTS)
+        .toArray();
+
+    int[] fractions = random.ints(0, 100)
+        .limit(NR_OF_AMOUNTS)
+        .toArray();
+
+    amounts = new String[NR_OF_AMOUNTS];
+    for (int i = 0; i < integers.length; i++) {
+      amounts[i] = integers[i] + "." + fractions[i];
+    }
+  }
+
+  @NotNull
+  public static BigDecimal randomAmount() {
+    return new BigDecimal(amounts[random.nextInt(amounts.length)]);
+  }
+
+  public static String randomAccount() {
+    return accounts[random.nextInt(accounts.length)];
+  }
+
+}
--- src/main/java/com/github/ferstl/processing/MessageProducer.java
+++ src/main/java/com/github/ferstl/processing/MessageProducer.java
@@ -2,58 +2,32 @@ package com.github.ferstl.processing;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Random;
-import org.jetbrains.annotations.NotNull;
 import com.github.ferstl.processing.Message.Metadata;
 import com.github.ferstl.processing.model.Payment;
-import static com.github.ferstl.processing.AccountMasterdata.getRegularAccountsAsArray;
+import static com.github.ferstl.processing.AccountUtil.randomAccount;
+import static com.github.ferstl.processing.AccountUtil.randomAmount;
 import static java.util.UUID.randomUUID;
 
 public class MessageProducer {
-
-  private static final int NR_OF_AMOUNTS = 10000;
-  private final String[] accounts;
-  private final String[] amounts;
-  private final Random random;
 
   private final MessageService messageService;
 
 
   public MessageProducer(MessageService messageService) {
     this.messageService = messageService;
-    this.random = new Random(42);
-    this.accounts = getRegularAccountsAsArray();
-
-    int[] integers = this.random.ints(100, 10000)
-        .limit(NR_OF_AMOUNTS)
-        .toArray();
-
-    int[] fractions = this.random.ints(0, 100)
-        .limit(NR_OF_AMOUNTS)
-        .toArray();
-
-    this.amounts = new String[NR_OF_AMOUNTS];
-    for (int i = 0; i < integers.length; i++) {
-      this.amounts[i] = integers[i] + "." + fractions[i];
-    }
   }
 
   public void producePayment(Message message) {
-    String debtorAccount = getAccount();
-    String creditorAccount = getAccount();
-    BigDecimal amount = getAmount();
+    String debtorAccount = randomAccount();
+    String creditorAccount = randomAccount();
+    BigDecimal amount = randomAmount();
 
     byte[] paymentData = this.messageService.writePayment(new Payment(debtorAccount, creditorAccount, amount));
     message.setData(paymentData);
     message.setMetadata(new Metadata(Instant.now(), randomUUID()));
   }
 
-  @NotNull
-  private BigDecimal getAmount() {
-    return new BigDecimal(this.amounts[this.random.nextInt(this.amounts.length)]);
-  }
-
-  private String getAccount() {
-    return this.accounts[this.random.nextInt(this.accounts.length)];
+  public Payment producePayment(byte[] data) {
+    return this.messageService.readPayment(data);
   }
 }
